5dde39150925d8d68a79c28e6fa2227c66927608
Griffin Young <griffin.young@schrodinger.com>
2024-09-07 21:53:36 -0400

    DEBUG=True to debug

--- lliza/lliza/settings.py
+++ lliza/lliza/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['polls-service-ijjp4culha-uc.a.run.app', 'lliza-production.up.railway.app']
 
