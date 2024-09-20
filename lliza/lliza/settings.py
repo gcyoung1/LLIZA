b5575c89c089f73550384ae6a08b72f9d0d99d3f
Griffin Young <griffin.young@schrodinger.com>
2024-09-20 01:45:13 -0400

    Add migrations

--- lliza/lliza/settings.py
+++ lliza/lliza/settings.py
@@ -73,7 +73,7 @@ WSGI_APPLICATION = "lliza.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_PUBLIC_URL")
 
 DATABASES = {
     "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
