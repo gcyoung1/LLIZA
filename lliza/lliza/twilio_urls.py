ba27fcb8f7c131e58933cc0afcd98be95ff9d1df
Griffin Young <griffin.young@schrodinger.com>
2024-11-05 17:17:40 -0500

    TWILIO_urls

--- lliza/lliza/twilio_urls.py
+++ lliza/lliza/twilio_urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from lliza.twilio_views import webhook, health
+from lliza.twilio_views import webhook, health, message_status
 
 urlpatterns = [
     path('webhook', webhook, name='webhook'),
     path('health', health, name='health'),
+    path('message-status', message_status, name='message-status'),
 ]
--- lliza/lliza/urls.py
+++ lliza/lliza/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     # ...
     path('webhook', webhook, name='webhook'),
     path('health', health, name='health'),
-    path('message-status', message_status, name='message-status'),
     # ...
 ]
