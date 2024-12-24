5d89faebe609851b138c155a07a5616af6bc0a55
Griffin Young <griffin.young@schrodinger.com>
2024-12-24 11:12:03 -0500

    Fix send_intro_message import

--- lliza/lliza/twilio_views.py
+++ lliza/lliza/twilio_views.py
@@ -315,7 +315,7 @@ def schedule_webhook(request):
         log_message(f"First day: {first_day}, first time: {first_time}")
         log_message(f"First cron string: {first_cron_string}")
         tasks.schedule(
-            "lliza.lliza.twilio_views.send_intro_message",
+            "lliza.twilio_views.send_intro_message",
             user_id,
             schedule_type="C",
             cron=first_cron_string
@@ -329,7 +329,7 @@ def schedule_webhook(request):
         second_cron_string = day_and_time_to_utc_cron_str(second_day, second_time)
         log_message(f"Second cron string: {second_cron_string}")
         tasks.schedule(
-            "lliza.lliza.twilio_views.send_intro_message",
+            "lliza.twilio_views.send_intro_message",
             user_id,
             schedule_type="C",
             cron=second_cron_string
